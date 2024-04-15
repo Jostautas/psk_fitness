f2a9fe9532554b436fe18c32055617b76f5a1be5
jostautas <jostautas.sakas@gmail.com>
2024-04-15 16:01:48 +0300

    Custom identity

--- psk_fitness/psk_fitness/Data/ApplicationUser.cs
+++ psk_fitness/psk_fitness/Data/ApplicationUser.cs
@@ -5,6 +5,9 @@ namespace psk_fitness.Data
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public int? Height { get; set; }
+        public double? Weight { get; set; }
+        public int? Age { get; set; }
     }
 
 }
