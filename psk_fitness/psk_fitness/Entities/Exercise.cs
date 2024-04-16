f7c8b69060ef747d83331b95af282c80e6ce62c7
Linas490 <113028160+Linas490@users.noreply.github.com>
2024-04-16 15:15:08 +0300

    Exercise class created

---
+++ psk_fitness/psk_fitness/Entities/Exercise.cs
@@ -0,0 +1,22 @@
+﻿namespace psk_fitness.Entities
+{
+    public class Exercise
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string WorkoutId { get; set; }
+        public string Description { get; set; }
+        public string Notes { get; set; }
+        public TimeSpan Duration { get; set; } // Stores hours, minutes, and seconds
+        public int Sets { get; set; }
+        public int Reps { get; set; }
+        public string Steps { get; set; } 
+
+        public bool IsExpanded { get; set; } = false;
+
+        public Exercise()
+        {
+            
+        }
+    }
+}
