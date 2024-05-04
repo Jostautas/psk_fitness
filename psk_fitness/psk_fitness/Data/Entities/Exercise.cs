77fcf58654066497e4934e7205905efcaf6ecd09
Linas490 <113028160+Linas490@users.noreply.github.com>
2024-05-04 21:19:42 +0300

    added DTO mapping

--- psk_fitness/psk_fitness/Data/Entities/Exercise.cs
+++ psk_fitness/psk_fitness/Data/Entities/Exercise.cs
@@ -9,7 +9,7 @@ namespace psk_fitness.Data
         [ForeignKey("Workout")]
         public required int WorkoutId { get; set; }
         public required string Title { get; set; }
-        public DateOnly? Duration { get; set; }
+        public TimeSpan? Duration { get; set; }
         public int? Sets { get; set; }
         public int? Reps { get; set; }
         public string Description { get; set; } = "";
--- psk_fitness/psk_fitness/MappingProfile.cs
+++ psk_fitness/psk_fitness/MappingProfile.cs
@@ -10,5 +10,17 @@ public class MappingProfile : Profile
     {
         CreateMap<Topic, TopicDisplayDTO>().ReverseMap();
         CreateMap<Topic, TopicCreateDTO>().ReverseMap();
+
+        CreateMap<ExerciseCreateDTO, Exercise>()
+            .ForMember(dest => dest.Duration, opt => opt.MapFrom(src => new DateTime(
+                src.DurationHours ?? 0,
+                src.DurationMinutes ?? 0,
+                src.DurationSeconds ?? 0
+            )));
+
+        CreateMap<Exercise, ExerciseDisplayDTO>()
+            .ForMember(dest => dest.DurationSeconds, opt => opt.MapFrom(src => src.Duration.HasValue ? (int?)src.Duration.Value.Hours : null))
+            .ForMember(dest => dest.DurationMinutes, opt => opt.MapFrom(src => src.Duration.HasValue ? (int?)src.Duration.Value.Minutes : null))
+            .ForMember(dest => dest.DurationHours, opt => opt.MapFrom(src => src.Duration.HasValue ? (int?)src.Duration.Value.Seconds : null));
     }
 }
