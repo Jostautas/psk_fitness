23b821ffb2c2b44977c44b8901f5b82e1619945b
Jostautas <69266048+Jostautas@users.noreply.github.com>
2024-05-06 12:11:55 +0300

    refractored

--- psk_fitness/psk_fitness/ClientServices/TopicFriendService.cs
+++ psk_fitness/psk_fitness/ClientServices/TopicFriendService.cs
@@ -15,18 +15,12 @@ public class TopicFriendService(HttpClient _httpClient) : ITopicFriendService
 {
     public async Task<List<int>> GetAllAccessibleTopicsAsync()
     {
-        //var response = await _httpClient.GetFromJsonAsync<List<Topic>>($"{Constants.ApiEndpointPrefix}/allAccessibleTopics");
-        //if (response == null)
-        //{
-        //    throw new Exception("Failed to load topics.");
-        //}
+        var TopicIdsToDisplay = await _httpClient.GetFromJsonAsync<List<int>>($"allAccessibleTopics");
+        if (TopicIdsToDisplay == null)
+        {
+            throw new Exception("Failed to load topics.");
+        }
 
-        var response = await _httpClient.GetAsync($"allAccessibleTopics");
-        Console.WriteLine("response:");
-        Console.WriteLine(response);
-
-        var json = await response.Content.ReadAsStringAsync();
-        var TopicIdsToDisplay = JsonSerializer.Deserialize<List<int>>(json);
         return TopicIdsToDisplay;
     }
 }
