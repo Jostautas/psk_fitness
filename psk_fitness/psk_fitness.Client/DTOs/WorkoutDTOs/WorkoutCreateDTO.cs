732a772d31fe1cde27455dfae86d46fdba408f55
Domantas1337 <domantas.kakc@gmail.com>
2024-05-07 19:02:10 +0300

    Added a form to check if it works

---
+++ psk_fitness/psk_fitness.Client/DTOs/WorkoutDTOs/WorkoutCreateDTO.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace psk_fitness.Client.DTOs.WorkoutDTOs
+{
+    public class WorkoutCreateDTO
+    {
+        public string Title { get; set; } = "No title";
+        public DateOnly Date { get; set; }
+        public TimeSpan? Duration { get; set; }
+        public string Description { get; set; } = "";
+        public string Notes { get; set; } = "";
+        public string FriendsNotes { get; set; } = "";
+        public bool Finished { get; set; } = false;
+
+    }
+}
